ec19999ba07ff02f6c632c1524f478889db37ba3
Npatro <87539390+Npatro@users.noreply.github.com>
2023-03-24 22:11:52 -0400

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,42 @@
+function setup(){
+    canvas= createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded );
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+img="";
+status="";
+
+function preload(){
+    img= loadImage('bedroom.png');
+}
+
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill('#FF0000');
+    text("Bed", 130, 215);
+    noFill();
+    stroke("#FF0000");
+    rect(120, 200, 350, 160);
+
+    fill('#FF0000');
+    text("Cushion", 455, 295);
+    noFill();
+    stroke("#FF0000");
+    rect(450, 280, 125, 75);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(){
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}q
